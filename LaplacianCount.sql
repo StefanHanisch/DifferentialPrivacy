21bba97b606724e8ec74b78c95e0118a6ff46eb1
Stefan Hanisch <mail@stefanhanisch.com>
2018-01-21 13:03:45 +0100

    Laplacian count T-SQL procedure

---
+++ LaplacianCount.sql
@@ -0,0 +1,54 @@
+CREATE PROCEDURE laplacianCount
+	-- name of the table, which is affected by 
+	-- the query
+	@table VARCHAR(MAX),
+	-- class attribute
+	@attribute VARCHAR(MAX), 
+	-- epsilon
+	@e FLOAT,			
+	--condition along the path in the tree			
+	@condition VARCHAR(MAX)	
+AS
+BEGIN
+	-- generate a random number by the uniform
+	-- distribution between 0 and 1
+	-- and shift it by - 0.5 to get values in 
+	-- the range of -0.5 and 0.5
+	DECLARE @random VARCHAR(12)
+	SET @random = ROUND(rand() - 0.5 , 10)
+	
+	-- b is the inverse of the epsilon parameter
+	DECLARE @b VARCHAR(20)
+	SET @b = ROUND( 1 / @e , 20)
+
+	-- if @random is equal to 0.5, the
+	-- argument of the log function is equals
+	-- to 0 and undefined. While @random is 
+	-- equals to 0.5, new random numbers 
+	-- are generated
+	WHILE (ABS(CAST(@random AS FLOAT)) = 0.5)
+		BEGIN
+			SET @random = ROUND(rand() - 0.5 , 10)
+		END
+
+	-- Execute the query and add Laplacian
+	-- noise to the outcome. The noisy 
+	-- result is rounded and casted to an
+	-- integer to get a natural number 
+	--	for the count query
+
+	-- The formula is: quantity - (1 / epsilon 
+	-- * sign (@random *
+	-- log ( 1 - 2 * Abs(@random))))
+	DECLARE @query VARCHAR(MAX)
+	SET @query = 'SELECT 
+	CAST(
+		ROUND
+			(COUNT(*) - (' + @b + '* sign(' + @random + ')) * LOG(1-2*ABS(' + @random + '))
+		,0)
+		 AS int) As quantity 
+	FROM ' + @table + ' 
+	WHERE ' + @condition
+
+	EXEC(@query)
+END
